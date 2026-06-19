3661a42b09c1b38ba87254f983aec533fa3c3161
bayjidmia <mdbayjidmia982@gmail.com>
2026-06-19 18:02:54 +0600

    create Users table

---
+++ schema/schema.sql
@@ -0,0 +1,11 @@
+
+-- users schema
+CREATE TABLE Users (
+    user_id INT  PRIMARY KEY,
+    full_name VARCHAR(100),
+    email VARCHAR(100),
+    role VARCHAR(50),
+    phone_number VARCHAR(20)
+);
+
+--Matches schema
