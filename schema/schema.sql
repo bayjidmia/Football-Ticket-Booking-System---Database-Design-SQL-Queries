a49d0d6d07bfc08e749eb5677b4be0a1db5dc9b8
bayjidmia <mdbayjidmia982@gmail.com>
2026-06-19 19:30:40 +0600

    created all queries and database

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,11 +1,30 @@
 
--- users schema
+-- 1. Create Users Table
 CREATE TABLE Users (
-    user_id INT  PRIMARY KEY,
-    full_name VARCHAR(100),
-    email VARCHAR(100),
-    role VARCHAR(50),
+    user_id INT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    role VARCHAR(50) NOT NULL,
     phone_number VARCHAR(20)
 );
 
---Matches schema
+-- 2. Create Matches Table
+CREATE TABLE Matches (
+    match_id INT PRIMARY KEY,
+    fixture VARCHAR(150) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price INT NOT NULL,
+    match_status VARCHAR(50) NOT NULL
+);
+
+-- 3. Create Bookings Table
+CREATE TABLE Bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    match_id INT NOT NULL,
+    seat_number VARCHAR(10),
+    payment_status VARCHAR(50),
+    total_cost INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id) ,
+    FOREIGN KEY (match_id) REFERENCES Matches(match_id)
+);
